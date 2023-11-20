0020195791300dcf27a7957f0325eb3c622b3fab
Snoda2023 <tk220137syotaro.noda@gmail.com>
2023-11-20 12:25:07 +0900

    variable_register_add

--- main.py
+++ main.py
@@ -5,7 +5,9 @@
 import ambient
 import requests
 import grove_gesture_sensor # ジェスチャーセンサ
+import grove_rgb_lcd
 from tm1637 import TM1637
+
 from time import sleep
 from os import getenv
 from dotenv import load_dotenv
@@ -18,8 +20,9 @@ write_key = getenv("WRITE_KEY")
 gest_convert = ["NOTHING","FORWARD", "BACKWARD", "RIGHT", "LEFT", "UP", "DOWN", "CLOCKWISE", "ANTI-CLOCWISE", "WAVE"]
 
 # インスタンスを生成するところ
-ambi = ambient.Ambient(channel_id, write_key)
-gest = grove_gesture_sensor.gesture()
+ambi_inst = ambient.Ambient(channel_id, write_key)
+gest_inst = grove_gesture_sensor.gesture()
+tm1637_inst = TM1637(clk=26,dio=19)
 
 # Ambientにデータを送る関数
 def send_ambient(ambi_inst,json_data:dict):
@@ -33,7 +36,9 @@ def send_ambient(ambi_inst,json_data:dict):
 # 処理の本体
 try:
     while True:
-        gesture_result = gest_convert[gest.return_gesture()] # ジェスチャの戻り値を文字列に変換
+        gesture_result = gest_convert[gest_inst.return_gesture()] # ジェスチャの戻り値を文字列に変換
+        
+        
         
         
 except KeyboardInterrupt:
