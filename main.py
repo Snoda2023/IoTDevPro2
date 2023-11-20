7e84c90027581bafb97d7a0aa039cf39cd682f5b
Snoda2023 <tk220137syotaro.noda@gmail.com>
2023-11-20 12:15:33 +0900

    first commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import ambient
 import requests
+import grove_gesture_sensor # ジェスチャーセンサ
+from tm1637 import TM1637
 from time import sleep
 from os import getenv
 from dotenv import load_dotenv
@@ -13,8 +15,11 @@ load_dotenv()
 channel_id = getenv("CHANNEL_ID")
 write_key = getenv("WRITE_KEY")
 
+gest_convert = ["NOTHING","FORWARD", "BACKWARD", "RIGHT", "LEFT", "UP", "DOWN", "CLOCKWISE", "ANTI-CLOCWISE", "WAVE"]
+
 # インスタンスを生成するところ
 ambi = ambient.Ambient(channel_id, write_key)
+gest = grove_gesture_sensor.gesture()
 
 # Ambientにデータを送る関数
 def send_ambient(ambi_inst,json_data:dict):
@@ -25,5 +30,13 @@ def send_ambient(ambi_inst,json_data:dict):
         print(f">> Request failed: {e}")
         return e
     
+# 処理の本体
+try:
+    while True:
+        gesture_result = gest_convert[gest.return_gesture()] # ジェスチャの戻り値を文字列に変換
+        
+        
+except KeyboardInterrupt:
+    print("interrupted!")
 
     
