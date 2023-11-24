367e6ea156f2b454cba55eb1b2665287562b465e
Snoda2023 <tk220137syotaro.noda@gmail.com>
2023-11-24 14:36:56 +0900

    cds_to_rgb

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ from dotenv import load_dotenv
 
 # 変数
 ambi_cnt:int = 0
+gest_convert = ["NOTHING","FORWARD", "BACKWARD", "RIGHT", "LEFT", "UP", "DOWN", "CLOCKWISE", "ANTI-CLOCWISE", "WAVE"]
 
 # 環境変数を取得
 load_dotenv()
 channel_id = getenv("CHANNEL_ID")
 write_key = getenv("WRITE_KEY")
 
-gest_convert = ["NOTHING","FORWARD", "BACKWARD", "RIGHT", "LEFT", "UP", "DOWN", "CLOCKWISE", "ANTI-CLOCWISE", "WAVE"]
 
 # インスタンスを生成するところ
 ambi_inst = ambient.Ambient(channel_id, write_key)
@@ -41,6 +41,13 @@ def send_ambient(ambi_inst,json_data:dict):
         print(f">> Request failed: {e}")
         return e
 
+def cds_to_rgb(value):
+    if value <= 1:
+        return [0,0,255]
+    else:         
+        red = 255 * (value // 3000)
+        blue = 255 * (1 - (value // 3000))
+        return [red,0,blue] # R G B
     
 # 処理の本体
 try:
@@ -50,9 +57,12 @@ try:
         
         us_dist = ultrasonic_inst.get_distance()                  # 超音波センサから距離を取得
         CdS = readadc(0)                                          # 光センサからの明るさ
-        Potentiomater = readadc(1)                                # 半固定抵抗の値
-        JoyStick_X = readadc(2)                                   # ジョイスティックのX軸値
+        Potentiomater = readadc(3)                                # 半固定抵抗の値
+        JoyStick_X = readadc(1)                                   # ジョイスティックのX軸値
         print(f"ジェスチャー:{us_dist}\nCdS:{CdS}\n半固定抵抗:{Potentiomater}\nJoyStick_X:{JoyStick_X}")
+        
+        
+        grove_lcd_inst.setRGB(cds_to_rgb(CdS))
         
         tm1637_inst.number(Potentiomater)                         # TM1637に半固定抵抗の値を書き込み
         
@@ -60,8 +70,8 @@ try:
         #     send_ambient(ambi_inst, {"d1":us_dist})
         #     ambi_cnt = 0
             
-        ambi_cnt += 2
-        sleep(2)
+        ambi_cnt += 3
+        sleep(3)
 except KeyboardInterrupt:
     print("interrupted!")
 
